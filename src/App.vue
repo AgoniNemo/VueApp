bfd56b5f2b04c0c95db4e412da26296cd83ecc6b
AgoniNemo <AgoniNemo@gmail.com>
2018-01-05 18:23:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-header>
+
+    </v-header>
+  </div>
+</template>
+
+<script>
+  import header from './components/Header.vue'
+  
+  export default {
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style>
+
+</style>
