2002430fca9bab8057b3750cfc5c034a27d38d5e
AgoniNemo <AgoniNemo@gmail.com>
2018-01-12 18:04:03 +0800

    add detail mail

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -17,13 +17,27 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
-
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" height="100%" width="100%">
+    </div>
+    <div v-show="detailShow"class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <p>{{seller.name}}</p>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close" @click="closeDetail"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +50,19 @@
         tyte: Object
       }
     },
+    data() {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail() {
+         this.detailShow = true;
+      },
+      closeDetail() {
+         this.detailShow = false;
+      }
+    },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
@@ -45,8 +72,10 @@
     @import "../../common/stylus/mixin"
 
     .header
+      position: relative
+      overflow: hidden
       color: #fff
-      background: #999
+      background: rgba(7,17,27,0.5)
       .content-wrapper
         position: relative
         padding: 24px 12px 18px 24px
@@ -118,5 +147,62 @@
             margin-left: 2px
             font-size: 10px
             line-height: 24px
+      .bulletin-wrapper
+        position: relative
+        height: 28px
+        line-height: 28px
+        padding: 0 22px 0 12px
+        white-space: nowrap
+        overflow: hidden
+        text-overflow: ellipsis
+        background: rgba(7,17,27,0.2)
+        .bulletin-title
+          display: inline-block
+          vertical-align: top
+          margin-top: 8px
+          width: 22px
+          height: 12px
+          bg-image('../../assets/header/bulletin')
+          background-size: 22px 12px
+          background-repeat: no-repeat
+        .bulletin-text
+          vertical-align: top
+          font-size: 10px
+          margin: 0 4px
+        .icon-keyboard_arrow_right
+          position: absolute
+          font-size: 10px
+          right: 12px
+          top: 8px
+      .background
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        z-index: -1
+        filter: blur(10px)
+      .detail
+        position: fixed
+        z-index: 100
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        overflow: auto
+        background: rgba(7,17,27,0.8)
+        .detail-wrapper
+          min-height: 100%
+          .detail-main
+            margin-top: 64px
+            padding-bottom: 64px
+        .detail-close
+          position: relative
+          width: 32px
+          height: 32px
+          margin: -64px auto 0 auto
+          clear: both
+          font-size: 32px
+
 
 </style>
