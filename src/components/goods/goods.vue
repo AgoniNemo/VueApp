608cbbfc066eff9dbb02175d3cfe00a6ab4e356a
AgoniNemo <XZC168520@163.com>
2018-01-17 18:17:58 +0800

    添加goods界面

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="goods">
     <div class="menu-wrapper">
-
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
     </div>
     <div class="foods-wrapper">
-
+        <ul>
+          <li v-for="item in goods" class="foot-list">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="food in item.foods" class="food-item">
+                <div class="icon">
+                  <img :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span>月售{{food.sellerCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span>¥ {{food.price}}</span>
+                    <span v-show="food.oldPrice">¥ {{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
     </div>
   </div>
 </template>
@@ -24,21 +54,23 @@
       };
     },
     created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.$http.get('api/goods').then((response) => {
         let resp = response.data;
         if (resp.errno === ERR_OK) {
           this.goods = resp.data;
         };
-        console.log(this.goods);
       })
       .catch(function (error) {
-        console.log('error',error);
+        console.log('error' + error);
       });
     }
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
+
   .goods
     display: flex
     position: absolute
@@ -50,6 +82,37 @@
       flex: 0 0 80px
       width: 80px
       background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        padding: 0 12px
+        line-height: 14px
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('../../assets/goods/decrease_3')
+          &.discount
+            bg-image('../../assets/goods/discount_3')
+          &.guarantee
+            bg-image('../../assets/goods/guarantee_3')
+          &.invoice
+            bg-image('../../assets/goods/invoice_3')
+          &.special
+            bg-image('../../assets/goods/special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(7,17,27,0.1))
+          font-size: 12px
+
     .foods-wrapper
       flex: 1
 
