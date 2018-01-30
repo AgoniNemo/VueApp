96eda8c5a9c6a7afe358abc11a9d5e188f6fcca1
AgoniNemo <XZC168520@163.com>
2018-01-30 17:53:46 +0800

    add ratingselect

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -27,6 +27,16 @@
           <h1 class="title">商品信息</h1>
           <p class="text">{{food.info}}</p>
         </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+          <div class="rating-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </transition>
@@ -36,8 +46,13 @@
   import price from 'components/price/price';
   import BScroll from 'better-scroll';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import ratingselect from 'components/ratingselect/ratingselect';
   import Vue from 'vue';
   import split from 'components/split/split';
+
+  // const POSITIVE = 0;
+  // const NEGATIVE = 1;
+  const ALL = 2;
 
   export default {
     props: {
@@ -47,14 +62,24 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show() {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = true;
         // nextTick里才能保证DOM是渲染的
         this.$nextTick(() => {
+          console.log(this.$refs['food']);
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs['food'], {
                 click: true
@@ -83,7 +108,8 @@
     components: {
       price,
       cartcontrol,
-      split
+      split,
+      ratingselect
     }
   };
 </script>
@@ -166,5 +192,26 @@
         opacity: 1
       .buy.v-leave-active
         opacity: 0
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7,17,27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77,85,93)
+    .rating
+      padding-top: 18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: 14px
+        color: rgb(7,17,27)
+
+
 
 </style>
